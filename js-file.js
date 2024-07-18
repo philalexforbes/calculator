eb495fab94473760532d365c0641290bb69dd28d
Phil <philalexforbes@gmail.com>
2024-07-18 12:11:29 -0500

    committing working calculator

--- js-file.js
+++ js-file.js
@@ -1,10 +1,44 @@
 const display = document.querySelector('#display');
-const backSpaceBtn = document.querySelector('#backSpacBtn');
+const backSpaceBtn = document.querySelector('#backSpaceBtn');
 const clearBtn = document.querySelector('#clearBtn');
 const equalBtn = document.querySelector('#equalBtn');
 const decimalBtn = document.querySelector('#decimalBtn');
 const numberBtns = document.querySelectorAll('.number');
 const operatorBtns = document.querySelectorAll('.operator');
+
+let currentNumber = '';
+let previousNumber = '';
+let operator = '';
+
+numberBtns.forEach((numberBtn) => {
+    numberBtn.addEventListener('click', (e) => {
+        displayNumber(e.target.value);
+        display.textContent = currentNumber;
+    });
+});
+
+operatorBtns.forEach((operatorBtn) => {
+    operatorBtn.addEventListener('click', (e) => {
+        getOperator(e.target.value);
+        display.textContent = operator;
+    });
+});
+
+backSpaceBtn.addEventListener('click', () => {
+    backSpace();
+});
+
+clearBtn.addEventListener('click', ()=> {
+    clearScreen();
+});
+
+equalBtn.addEventListener('click', ()=> {
+    compute();
+});
+
+decimalBtn.addEventListener('click', ()=>{
+    decimalLogic();
+});
 
 const add = function(num1, num2) {
     return num1 + num2;
@@ -20,68 +54,65 @@ const multiply = function(num1, num2) {
 
 const divide = function(num1, num2) {
     if(num2 === 0){
-        return 'Oh no! The fabric of the universe!';
+        return 'Oh no!';
     }
     return num1 / num2;
 }
 
-const getFirstNumber = function(){
-    let firstNumber = '';
-    numberBtns.forEach((numberBtn) => {
-        numberBtn.addEventListener('click', (e) => {
-            firstNumber += '' + e.target.value;
-            display.textContent = firstNumber;
-            console.log(`firstNumber: ${firstNumber}`);
-        });
-    });
-    return firstNumber = Number(firstNumber);
+const displayNumber = function (num) {
+    currentNumber += num;
 }
 
-const getSecondNumber = function(){
-    let secondNumber = '';
-    numberBtns.forEach((numberBtn) => {
-        numberBtn.addEventListener('click', (e) => {
-            secondNumber += '' + e.target.value;
-            display.textContent = secondNumber;
-            console.log(`secondNumber: ${secondNumber}`);
-        });
-    });
-    return secondNumber = Number(secondNumber);
+const clearScreen = function () {
+    display.textContent = '0';
+    currentNumber = '';
+    operator = '';
+    previousNumber = '';
 }
 
-const getOperator = function() {
-    let operator = '';
-    operatorBtns.forEach((operatorBtn) => {
-        operatorBtn.addEventListener('click', (e) => {
-            operator = e.target.value;
-            console.log(operator);
-            display.textContent = operator;
-        });
-    });
-    return operator; 
+const getOperator = function(op) {
+    operator = op;
+    previousNumber = currentNumber;
+    currentNumber = '';
 }
 
-const compute = function(number1, operator, number2 ) {
+const backSpace = function() {
+    currentNumber = currentNumber.slice(0,-1);
+    display.textContent = (display.textContent.slice(0,-1));
+    if (currentNumber === ''){
+        display.textContent = '0';
+    } else if(currentNumber && previousNumber !== '') {
+
+    }
+}
+
+const decimalLogic = function() {
+    if (currentNumber.includes('.')) {
+        currentNumber = currentNumber;
+    } else {
+        currentNumber += '.'
+    }
+}
+
+const compute = function() {
+    let number1 = Number(previousNumber);
+    let number2 = Number(currentNumber);
     let computedValue = 0;
-    equalBtn.addEventListener('click', () => {
-        if (operator === '+') {
-            computedValue = add(number1, number2);
-        } else if (operator === '-'){
-                computedValue = subtract(number1, number2);
-        } else if (operator === '*'){
-                computedValue = multiply(number1, number2);
-        } else if (operator === '/'){
-                computedValue = divide(number1, number2);
-        }
-        display.textContent = computedValue;
-    });
+    if (operator === '+') {
+        computedValue = add(number1, number2);
+    } else if (operator === '-'){
+        computedValue = subtract(number1, number2);
+    } else if (operator === '*'){
+        computedValue = multiply(number1, number2);
+    } else if (operator === '/'){
+        computedValue = divide(number1, number2);
+    }
+    computedValue = Math.round(computedValue * 100000) / 100000;
+    display.textContent = computedValue;
 }
 
 const operate = function() {
-    let number1 = getFirstNumber();
-    let operator = getOperator();
-    let number2 = getSecondNumber();
-    compute(number1, operator, number2);
+    compute();
 }
 
 
